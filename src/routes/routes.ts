e6458d9da32a62ceb45ff2f3cbb4f2d456c512e3
Matt Rivas <matt.rivas202@gmail.com>
2022-05-23 20:42:03 -0700

    removed wallet connect, abstracted nft abi, send successful mint response

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -2,16 +2,27 @@ import { Request, Response } from "express";
 import { ethers, providers } from "ethers";
 import WalletConnectProvider from "@walletconnect/web3-provider";
 require("dotenv").config();
-const provider = new WalletConnectProvider({ infuraId: process.env.infuraId });
+// const provider = new WalletConnectProvider({ infuraId: process.env.infuraId });
 
+const abi = [{}];
+const nftMintContract = "";
 export interface RequestArguements {
   method: string;
   params?: unknown[] | object;
+  chainId: number;
+  mintAddress: string;
 }
 export async function mint(req: Request, resp: Response) {
-  await provider.enable();
-  const web3Provider = new providers.Web3Provider(provider);
-  const request: RequestArguements = req.body;
-  const result = await provider.request(request);
-  return result;
+  //FIXME verify mintaddress is checksum
+  const request = req.body;
+  const { chainId, mintAddress } = request;
+  const provider = new ethers.providers.InfuraProvider(
+    chainId,
+    process.env.infuraId
+  );
+  //todo create contract instance and mint to address provided
+
+  const contract = new ethers.Contract(nftMintContract, abi, provider);
+  const res = contract.mint(mintAddress);
+  resp.send(res);
 }
